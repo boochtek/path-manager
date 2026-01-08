3567786b075f6dc476bf79a2211d12e17ba9adf1
Craig Buchek <craig@boochtek.com>
2026-01-07 16:27:53 -0800

    Update Homebrew formula to 0.9.2

--- Formula/path.rb
+++ Formula/path.rb
@@ -1,8 +1,8 @@
 class Path < Formula
   desc "Easily manipulate your shell PATH"
   homepage "https://github.com/boochtek/path-manager"
-  url "https://github.com/boochtek/path-manager/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "1c7fbe810ee95ee0a4babd3dc36da3d208f5d8f1a7a26a68dba00511c1525136"
+  url "https://github.com/boochtek/path-manager/archive/refs/tags/v0.9.2.tar.gz"
+  sha256 "b9b705b82447f240f2cad19e96c56441915d7230c8594717343816b65d387ad5"
   license "MIT"
   head "https://github.com/boochtek/path-manager.git", branch: "main"
 
